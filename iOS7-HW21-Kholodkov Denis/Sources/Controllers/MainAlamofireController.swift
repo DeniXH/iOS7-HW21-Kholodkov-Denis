e3c3cfb65e57810a7ca19279e6dd21a51231cf50
Denis <den88000@gmail.com>
2022-11-02 11:59:25 +0300

    Initial Commit

---
+++ iOS7-HW21-Kholodkov Denis/Sources/Controllers/MainAlamofireController.swift
@@ -0,0 +1,19 @@
+//
+//  MainAlamofireController.swift
+//  iOS7-HW21-Kholodkov Denis
+//
+//  Created by Денис Холодков on 02.11.2022.
+//
+
+import UIKit
+import Alamofire
+
+class MainAlamofireController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+}
+
